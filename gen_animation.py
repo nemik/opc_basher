6c03b9b5e9947e44eb56ccf271e7b6c9bcb5d378
Nemanja Stefanovic <nemik@nemik.net>
2015-03-16 17:41:46 -0500

    proper extensions

--- gen_animation.py
+++ gen_animation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # edit these to what you want
 # number of LEDs in your chain (only supports one channel up to 64 for now)
 num_leds = 40
@@ -20,14 +21,14 @@ for i in range(0,num_leds):
     # red
     if i % 2 == 0:
         # r for frames[0]
-        frames[0] = frames[0]+("\\x%02x\\x00\\x00" % (darken * 255))
+        frames[0] = frames[0]+("\\x%02x\\x%02x\\x%02x" % (darken * 255, 0, 0)) #red
         # g for frames[1]
-        frames[1] = frames[1]+("\\x00\\x%02x\\x00" % (darken * 255))
+        frames[1] = frames[1]+("\\x%02x\\x%02x\\x%02x" % (0, darken * 255, 0)) #green
     else:
         # g for frames[0]
-        frames[0] = frames[0]+("\\x00\\x%02x\\x00" % (darken * 255))
+        frames[0] = frames[0]+("\\x%02x\\x%02x\\x%02x" % (0, darken * 255, 0)) #green
         # r for frames[1]
-        frames[1] = frames[1]+("\\x%02x\\x00\\x00" % (darken * 255))
+        frames[1] = frames[1]+("\\x%02x\\x%02x\\x%02x" % (darken * 255, 0, 0)) #red
 
 outbash = "while true; do\n"
 for i in range(0,num_frames):
